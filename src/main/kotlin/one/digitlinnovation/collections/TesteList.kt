34c9c4477a42c56ea6c1f5b3f90052c2a38ddd53
Vinicius Hilário <vinihdeo@gmail.com>
2022-08-02 17:41:25 -0300

    Testando o groupBy

--- src/main/kotlin/one/digitlinnovation/collections/TesteList.kt
+++ src/main/kotlin/one/digitlinnovation/collections/TesteList.kt
@@ -2,9 +2,9 @@ package one.digitlinnovation.collections
 
 fun main() {
 
-    val joao = Funcionarios("João", 2000.0)
-    val pedro = Funcionarios("Pedro", 1500.0)
-    val maria = Funcionarios("Maria", 4000.0)
+    val joao = Funcionarios("João", 2000.0, "CLT")
+    val pedro = Funcionarios("Pedro", 1500.0, "PJ")
+    val maria = Funcionarios("Maria", 4000.0, "CLT")
 
     //Criando um exemplo de List
     val funcionarios = listOf(joao, pedro, maria)
@@ -12,17 +12,24 @@ fun main() {
     funcionarios.forEach{ println(it) }
 
     println("---------------")
+    //Encontrando um tipo de elemento na list usando .find()
     println(funcionarios.find { it.nome == "Maria" })
 
     println("---------------")
+    //Oredenando os salarios da list usando .sortedBy()
     funcionarios.sortedBy { it.salario }.forEach{ println(it) }
+
+    println("---------------")
+    //Criando grupo de acordo com o tipoContrataçao usando .groupBy()
+    funcionarios.groupBy { it.tipoContratacao }.forEach{ println(it) }
 
 }
 //Criando uma data class para teste
 data class Funcionarios(
 
     val nome: String,
-    val salario: Double
+    val salario: Double,
+    val tipoContratacao: String
 
 ) {
 
