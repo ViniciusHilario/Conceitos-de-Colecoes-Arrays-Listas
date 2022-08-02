0e39056d622a247de64ffbc35c27a0e1cde4ff27
Vinicius Hilário <vinihdeo@gmail.com>
2022-08-02 17:20:17 -0300

    Testando o sortedBy

--- src/main/kotlin/one/digitlinnovation/collections/TesteList.kt
+++ src/main/kotlin/one/digitlinnovation/collections/TesteList.kt
@@ -2,8 +2,8 @@ package one.digitlinnovation.collections
 
 fun main() {
 
-    val joao = Funcionarios("João", 1000.0)
-    val pedro = Funcionarios("Pedro", 2000.0)
+    val joao = Funcionarios("João", 2000.0)
+    val pedro = Funcionarios("Pedro", 1500.0)
     val maria = Funcionarios("Maria", 4000.0)
 
     //Criando um exemplo de List
@@ -13,6 +13,10 @@ fun main() {
 
     println("---------------")
     println(funcionarios.find { it.nome == "Maria" })
+
+    println("---------------")
+    funcionarios.sortedBy { it.salario }.forEach{ println(it) }
+
 }
 //Criando uma data class para teste
 data class Funcionarios(
@@ -22,11 +26,9 @@ data class Funcionarios(
 
 ) {
 
-    override fun toString(): String = """
-        
+    override fun toString(): String = """        
         Nome:    $nome
-        Salário: $salario
-        
+        Salário: $salario        
     """.trimIndent()
 
 }
